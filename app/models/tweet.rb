73e1e69af2bb91dc620658999b799ac39d6c481c
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-07-06 10:32:54 -0500

    Add Tweets table and set up relationships in Models

---
+++ app/models/tweet.rb
@@ -0,0 +1,18 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :integer          not null, primary key
+#  message    :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
