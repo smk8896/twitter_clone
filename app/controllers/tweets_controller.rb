8f1ed34fe65f78160b47fc237c18f39e1a383860
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-07-10 09:03:31 -0500

    Add ability to create a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,11 +6,31 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: "You just tweeted." }
+      else
+        format.html {render :new }
+      end
+    end
+
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
   end
 end
