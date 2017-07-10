df638f126b813fc282d524e07d207cbba60f14ca
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-07-10 10:04:38 -0500

    Ability to update a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet, only: [:edit, :show, :update]
 
   def index
     @tweets = Tweet.all
@@ -23,13 +24,31 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    # Get the tweet we want to edit from the model and send it to the view
+    # set_tweet before action gets the tweet we want to edit
+  end
+
+  def update
+    # Get the @tweet we want to update
+    # update it with the new info (from params)
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html {redirect_to @tweet, notice: "You changed that tweet"}
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
+    #set_tweet before action gets the tweet we want to show
   end
 
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:message, :user_id)
