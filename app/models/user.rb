0349e263aad44d03186924e04e5be6dee05d8ed2
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-07-11 10:17:21 -0500

    New Beautiful Profile Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :friends, through: :relationships
 
   # ~ Person being followed
-  has_many :inverse_relationships, class_name: "Relationship"
+  has_many :inverse_relationships, class_name: "Relationship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_relationships, source: :user
 
   # ~~ Validations ~~
